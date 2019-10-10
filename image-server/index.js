40d6e1b43baee88b8898f46366855270b5a9a5e7
Derek Nguyen <derek@penandpillow.com>
2019-10-10 11:48:18 +0900

    use port

--- image-server/index.js
+++ image-server/index.js
@@ -7,5 +7,6 @@ const app = new Koa()
 app.use(logger())
 app.use(serve(__dirname + '/images'))
 
-app.listen(3000)
-console.log('listening on port 3000')
+const PORT = process.env.PORT || 4000
+app.listen(PORT)
+console.log(`listening on port ${PORT}`)
